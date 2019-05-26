2d58b4bfd8da6c470d6d5c24a9e92856870405ea
Denis Nekrasov <denis.n@doka-it.ru>
2019-05-26 16:42:54 +0300

    Update rating-competitions.js

--- js/rating-competitions.js
+++ js/rating-competitions.js
@@ -8,15 +8,37 @@ class RatingCompetition {
     
     constructor (el, items, options)
     {
-        this.element = document.querySelector(el);
+        this.element = el;
         this.items = items;
         this.options = options;
+
+        if(options.maxValue){
+            this.maxValue = options.maxValue;
+        }
+        
         this.Render();
     }
 
+    maxValue = 100;
+
     Render()
     {
-        
+        let template = "";
+        for(let i = 0; i < this.items.length; i++){
+            template += 
+            `<div class="rating-item">`+
+                `<hr class="track" style="background-color:${this.items[i].roadColor};">`+
+                `<div class="car" style="left: ${this.items[i].value}%">`+
+                    `<p class="name">${this.items[i].name}</p>`+
+                    `<img src="${this.items[i].carPath}" alt="" class="racing-car">`+
+                    `<div class="tooltop">`+
+                    `</div>`+
+                `</div>`+
+                `<img src="img/finish.png" alt="" class="finish">`+
+                `<div class="leader"></div>`+
+            `</div>`
+        }
+        this.element.innerHTML = template;
     }
 
     /**
